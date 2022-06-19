bff7e2d97b92f6ac5deb57c595f88e0b87a2273b
tabassum2507 <10tabassum2@gmail.com>
2022-06-19 23:53:26 +0530

    D-2

--- slidingWindow/maxSum1.js
+++ slidingWindow/maxSum1.js
@@ -7,8 +7,9 @@ function maxSum( arr, num ){
     for(let i=0; i <arr.length - num+1; i++){
         temp=0;
 
-        for(let j=0; j<num; j++){
+        for(let j=1; j<num; j++){
             temp += arr[i+j];
+            console.log(temp)
         }
         if (temp > max) {
             max= temp;
