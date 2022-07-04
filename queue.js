143c15fb9c8503dbe47f545c496fe7d5b7ad0574
tabassum2507 <10tabassum2@gmail.com>
2022-07-04 23:57:23 +0530

    D-14

--- queue.js
+++ queue.js
@@ -5,28 +5,29 @@ class Node {
     }
 }
 
-class Stack {
+class Queue {
     constructor(){
         this.first = null;
         this.last = null;
         this.size = 0;
     }
-    push(val){
+    enqueue(val){
         var newNode = new Node(val);
         if(!this.first){
             this.first = newNode;
             this.last = newNode;
         } else {
-            var temp = this.first;
-            this.first = newNode;
-            this.first.next = temp;
+            this.last.next = newNode;
+            this.last = newNode;
         }
         return ++this.size;
     }
-    pop(){
+
+    dequeue(){
         if(!this.first) return null;
+
         var temp = this.first;
-        if(this.first === this.last){
+        if(this.first === this.last) {
             this.last = null;
         }
         this.first = this.first.next;
